94aa3db5efbfee171e346d87fa6a7b12a0e004fe
Kushal Kumarasinghe <kushal.k@hotmail.com>
2023-11-08 23:18:13 -0500

    adding more articles logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,5 +5,41 @@ class ArticlesController < ApplicationController
 
     def show
         @article = Article.find(params[:id])
-      end
+    end
+
+    def new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        article = Article.find(params[:id])
+
+        article.title = params[:title]
+        article.body = params[:body]
+        article.save
+
+        redirect_to articles_url
+    end
+
+    def create
+        title = params[:title]
+        body = params[:body]
+
+        article = Article.new(title: title, body: body)
+        article.save
+
+        redirect_to articles_url
+    end
+
+    def destroy
+        puts 'DELETING!!!!'
+        article = Article.find(params[:id])
+        article.destroy
+        redirect_to articles_url
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   root "articles#index"
 
-  get "/articles" => "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
   # Defines the root path route ("/")
   # root "posts#index"
 end
